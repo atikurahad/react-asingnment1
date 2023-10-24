1bb70b406f408b07e415f75e68503478a7c11a99
atikurahad <atikurahad@gmail.com>
2023-10-24 08:11:49 +0600

    style added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
       <h1> Hello I am from App </h1>
+      <Header />
+      <Footer/>
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <footer> &copy; All rights reserved by <span className="span" >DEV ZONE</span> </footer>
+        </div>
+    );
+}
+
+export default Footer;
